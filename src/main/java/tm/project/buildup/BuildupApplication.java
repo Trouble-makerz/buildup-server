5ed4f05934714f8dc139bae7cd644d9def9623c8
peter <97747863+wcorn@users.noreply.github.com>
2023-05-07 17:58:07 +0900

    openapi 수정

--- src/main/java/tm/project/buildup/BuildupApplication.java
+++ src/main/java/tm/project/buildup/BuildupApplication.java
@@ -1,5 +1,7 @@
 package tm.project.buildup;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,6 +13,11 @@ import java.util.TimeZone;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "/", description = "Default Server url")
+        }
+)
 public class BuildupApplication {
     @PostConstruct
     public void startedTimeZoneSet() {
