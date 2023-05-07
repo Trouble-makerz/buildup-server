56bfb3c02bf44674476047f3a89ee563ffcff60b
peter <97747863+wcorn@users.noreply.github.com>
2023-05-07 17:35:52 +0900

    test코드 주석처리

--- src/test/java/tm/project/buildup/BuildupApplicationTests.java
+++ src/test/java/tm/project/buildup/BuildupApplicationTests.java
@@ -3,11 +3,11 @@ package tm.project.buildup;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BuildupApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class BuildupApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
